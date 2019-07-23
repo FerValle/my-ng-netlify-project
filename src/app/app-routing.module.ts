68c9ee2936c89e1d0cad6f59a3e31ced8fa9ef8d
fer <fervalle264@gmail.com>
2019-07-23 17:32:44 -0300

    My Netlify Angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [];
 
@@ -7,4 +9,9 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'about', component: AboutComponent }
+  ];
+}
