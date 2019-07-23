587c59d1236b6e4d2ac44f02a4c58ea8fa307a6c
fer <fervalle264@gmail.com>
2019-07-23 17:55:29 -0300

    redireccionamiento y correccion router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
-  routes: Routes = [
-    { path: '', component: HomeComponent }, 
-    { path: 'about', component: AboutComponent }
-  ];
-}
+export class AppRoutingModule { }
